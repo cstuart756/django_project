a8835bfdccf97dd01070fa6649d87c5c4e765735
cstuart756 <stujjc@yahoo.co.uk>
2026-01-17 17:52:45 +0000

    Import HttpResponse in hello_world index view

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,8 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
+    return HttpResponse(request.method)
